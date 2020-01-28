a59221286033962bb114146e0136b604b374e0fa
whyo4o <renxio3o@gmail.com>
2020-01-28 09:16:38 +0900

    d

--- Necro/GameObject/Player.cpp
+++ Necro/GameObject/Player.cpp
@@ -6,17 +6,18 @@
 Player::Player(string name, D3DXVECTOR2 pos, D3DXVECTOR2 size)
 	:GameObject(name, pos, size)
 {
+	 moveType = MoveType_Control;
 
 	frameX = frameY = 0;
-	_pos = pos;
-	_size = size;
+	this->size = size;
+	position = pos;
+	imagePos = pos;
 	rc = FloatRect(pos, size, Pivot::CENTER);
 	destination = pos;
 	interver = 0;
 	head = "PlayerHeadRight";
 	body = "PlayerBodyRight";
 	startTime = 0;
-
 	currentState = nullptr;
 	stateList.insert(make_pair("Idle", new PlayerIdle(this)));
 	stateList.insert(make_pair("Move", new PlayerMove(this)));
@@ -50,6 +51,11 @@ void Player::ControlUpdate()
 	currentState->BeatExcute();
 }
 
+void Player::MissControlUpdate()
+{
+
+}
+
 void Player::Update(float tick)
 {
 	GameObject::Update(tick);
@@ -59,7 +65,7 @@ void Player::Update(float tick)
 
 	interver += tick * 4;
 
-	if (interver > 0.35f) {
+	if (interver > 0.5f) {
 		frameX++;
 		interver = 0;
 	}
@@ -67,19 +73,19 @@ void Player::Update(float tick)
 		frameX = 0;
 	}
 
-	rc = FloatRect(_pos, _size, Pivot::CENTER);
+	rc = FloatRect(position, size, Pivot::CENTER);
 }
 
 
 void Player::Render()
 {
 	
-	imagePos = _pos;
+	
 	// 이미지 위치 보정 (-20);
 	// 이미지만 점프 시킬꺼면 변수를 _포즈 말고 다른거 써야됨 . >> 포즈는 인덱스 검출하고 그래서 그냥 중점에 박혀있는게 좋기 때문.. 
-	_ImageManager->FindTexture("PlayerShadow")->Render(FloatRect(D3DXVECTOR2(_pos.x, _pos.y), D3DXVECTOR2(_size.x, _size.y / 4), Pivot::CENTER), nullptr);
-	_ImageManager->FindTexture(body)->FrameRender(FloatRect(D3DXVECTOR2(imagePos.x, imagePos.y - 20), _size, Pivot::CENTER), nullptr, frameX, frameY);
-	_ImageManager->FindTexture(head)->FrameRender(FloatRect(D3DXVECTOR2(imagePos.x, imagePos.y - 20), _size, Pivot::CENTER), nullptr, frameX, frameY);
+	_ImageManager->FindTexture("PlayerShadow")->Render(FloatRect(D3DXVECTOR2(position.x, position.y), D3DXVECTOR2(size.x, size.y / 4), Pivot::CENTER), nullptr);
+	_ImageManager->FindTexture(body)->FrameRender(FloatRect(D3DXVECTOR2(imagePos.x, imagePos.y - 20), size, Pivot::CENTER), nullptr, frameX, frameY);
+	_ImageManager->FindTexture(head)->FrameRender(FloatRect(D3DXVECTOR2(imagePos.x, imagePos.y - 20), size, Pivot::CENTER), nullptr, frameX, frameY);
 }
 
 void Player::ImguiRender()
@@ -87,11 +93,11 @@ void Player::ImguiRender()
 	ImGui::Begin("Info");
 	{
 		//ImGui::Text("Tick : %f", Time::Delta());
-		ImGui::Text("PosX : %.2f, PosY : %.2f", _pos.x, _pos.y);
+		ImGui::Text("PosX : %.2f, PosY : %.2f", position.x, position.y);
 		ImGui::Text("startTime : %.2f", startTime);
 		ImGui::Text("destinationX : %.2f, destinationY : %.2f", destination.x, destination.y);
 
-		POINT XY = PosToIndex(_pos, _GameWorld->GetTileManager()->GetTileSize(), _GameWorld->GetTileManager()->GetPivotPos());
+		POINT XY = PosToIndex(position, _GameWorld->GetTileManager()->GetTileSize(), _GameWorld->GetTileManager()->GetPivotPos());
 		ImGui::Text("Index : %d, %d", XY.x, XY.y);
 		ImGui::Text("Index-1 : %d, %d", XY.x - 1, XY.y - 1);
 		ImGui::Text("MapSize : %d, %d", _GameWorld->GetTileManager()->GetMapSize().x, _GameWorld->GetTileManager()->GetMapSize().y);
@@ -124,7 +130,7 @@ void PlayerIdle::BeatExcute()
 	// 여기서 타일을 검사한 뒤에 결과 값에 따라 move,attact,idle 중 하나로 이동 하면 됨 
 	// 무기 장착 하거나 했을때 상태변화를 어떻게 줘야 할까? >> 무기는 
 
-	me->myIndex = PosToIndex(me->_pos, _GameWorld->GetTileManager()->GetTileSize(), _GameWorld->GetTileManager()->GetPivotPos());
+	me->myIndex = PosToIndex(me->position, _GameWorld->GetTileManager()->GetTileSize(), _GameWorld->GetTileManager()->GetPivotPos());
 
 
 	if (KeyCode->Down(VK_LEFT))
@@ -144,9 +150,10 @@ void PlayerIdle::BeatExcute()
 			if (leftTilePos->GetAttribute() != ObjStatic)
 			{
 				me->startTime = 0;																					  // 시작 시간 초기화
-				me->startPos = me->_pos;																			  // 시작 위치 
+				me->startPos = me->position;																			  // 시작 위치 
 				//me->destination.x = me->_pos.x - _GameWorld->GetTileManager()->GetTileSize().x; >> 이렇게 계산을 이곳저곳에서 하지 말자. 
 				me->destination.x = leftTilePos->GetPos().x; // 목적지
+				me->jumpPos = D3DXVECTOR2(me->destination.x + 26, me->startPos.y - 40); 
 
 				me->ChangeState("Move");
 			}
@@ -167,8 +174,9 @@ void PlayerIdle::BeatExcute()
 			if (rightTilePos->GetAttribute() != ObjStatic)
 			{
 				me->startTime = 0;
-				me->startPos = me->_pos;
+				me->startPos = me->position;
 				me->destination.x = rightTilePos->GetPos().x;
+				me->jumpPos = D3DXVECTOR2(me->destination.x-26, me->startPos.y-40); 
 
 				me->ChangeState("Move");
 			}
@@ -184,8 +192,9 @@ void PlayerIdle::BeatExcute()
 			if (upTilePos->GetAttribute() != ObjStatic)
 			{
 				me->startTime = 0;
-				me->startPos = me->_pos;
+				me->startPos = me->position;
 				me->destination.y = upTilePos->GetPos().y;
+				me->jumpPos = D3DXVECTOR2(me->destination.x , me->startPos.y - 75); 
 
 				me->ChangeState("Move");
 			}
@@ -201,8 +210,9 @@ void PlayerIdle::BeatExcute()
 			if (downTilePos->GetAttribute() != ObjStatic)
 			{
 				me->startTime = 0;
-				me->startPos = me->_pos;
+				me->startPos = me->position;
 				me->destination.y = downTilePos->GetPos().y;
+				me->jumpPos = D3DXVECTOR2(me->destination.x, me->startPos.y - 10);
 
 				me->ChangeState("Move");
 			}
@@ -235,20 +245,23 @@ void PlayerMove::Excute()
 
 	me->startTime += Time::Tick();									// 0으로 초기화한 startTime에 tick을 더해라 
 
-	me->_pos = Math::Lerp(me->startPos, me->destination, factor);	// Lerp함수를 이용하여 목표 거리를(destination-startPos)  일정 비율(factor)로 이동
+	me->position = Math::Lerp(me->startPos, me->destination, factor);	// Lerp함수를 이용하여 목표 거리를(destination-startPos)  일정 비율(factor)로 이동
 
 	// y축 ㅏㄸ라 가야 하느디.. 
-
-	me->imagePos = Math::Lerp(me->startPos, me->destination, factor);
-	
-	if (factor <= 0.5f)												
+	// Lerp 안쓰는게 좋을듯. 미끄러지듯이 움직이게된다. 중력 넣어서 점프하는것처럼 만드는게 ㅇㅅㅇ.. 
+	// 점프 ㅎ야 한다 ㅣㅇ거.. 
+	if (factor <= 0.55f)												
 	{
-
+		me->imagePos = Math::Lerp(me->startPos, me->jumpPos, factor);
 	}
-	if (factor > 0.5f&&factor < 1.0f)
+	if (factor > 0.55f && factor < 1.0f)
 	{
-
+		me->imagePos = Math::Lerp(me->startPos, me->destination, factor);
 	}
+
+
+
+
 	if (factor >= 1.0f)												//비울(factor) >= 1  >>>> 목표지점까지 가는데 걸린 시간이 목표한 시간과 같거나 크다 = 도착했다
 	{
 		//me->_pos = me->destination;								// 위치 보정 할꺼면 위치보정 
